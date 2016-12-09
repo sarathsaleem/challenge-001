fde837d6b992068b1aa6c60a7f9773af74c00884
Sarath <sarathsaleem@gmail.com>
2016-12-10 03:16:39 +0400

    Add minified file and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: __dirname,
-        filename: 'app.min.js',
+        filename: './app.min.js',
     },
     module: {
         loaders: []
     },
-    plugins: [ ]
+     plugins: []
 };
